7497988eee625b32c00700ed5ecad442125954e7
Jacoder23 <Jacoder23@users.noreply.github.com>
2021-08-08 00:51:32 +0800

    v0.02 - DanmakU integration

---
+++ As Time Passed/Assets/SpellcardController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DanmakU;
+using DanmakU.Fireables;
+
+public class SpellcardController : MonoBehaviour
+{
+    Animator KosuzuAnimation;
+    GameObject KosuzuEmitter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        KosuzuAnimation = GameObject.Find("KosuzuController").GetComponent<Animator>();
+        KosuzuEmitter = GameObject.Find("DanmakuEmitter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (KosuzuAnimation.GetBool("Fire Bullet?"))
+        {
+            KosuzuEmitter.SetActive(true);
+        }
+        else
+        {
+            KosuzuEmitter.SetActive(false);
+        }
+    }
+}
