ff48e56497278648b5855dc272b7f8d7a65c0f2a
Jacoder23 <Jacoder23@users.noreply.github.com>
2021-08-07 20:18:59 +0800

    v0.01 - Basic movement and animations

---
+++ As Time Passed/Assets/Scripts/Systems/AttachToPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachToPlayer : MonoBehaviour
+{
+
+    public GameObject ObjectToAttachTo;
+    public bool changeScale;
+    public bool changeRotation = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ObjectToAttachTo.transform.position;
+        if (changeRotation)
+        {
+            transform.localRotation = ObjectToAttachTo.transform.localRotation;
+        }
+        if (changeScale)
+        {
+            transform.localScale = ObjectToAttachTo.transform.localScale;
+        }
+    }
+}
